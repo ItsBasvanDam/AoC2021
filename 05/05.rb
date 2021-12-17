f5b41b26af7da3650fcd351a8342fb353dac0c40
@bas.van.dam <bas.van.dam@sogeti.com>
2021-12-17 15:56:43 +0100

    Use Hash instead of [] because it is a bit faster

--- 05/05.rb
+++ 05/05.rb
@@ -1,6 +1,6 @@
 # part 1 & 2
 arr = File.readlines("input").map(&:chomp).map { |l| l.split(" -> ") }
-coords1, coords2 = [], []
+counts1, counts2 = Hash.new(0), Hash.new(0)
 arr.each do |set|
   p1 = set[0].split(",").map &:to_i
   p2 = set[1].split(",").map &:to_i
@@ -8,17 +8,17 @@ arr.each do |set|
   if p1[0] == p2[0] or p1[1] == p2[1]
     (c[0].min..c[0].max).each do |x|
       (c[1].min..c[1].max).each do |y|
-        coords1.push("#{x},#{y}")
-        coords2.push("#{x},#{y}")
+        counts1[[x, y]] += 1
+        counts2[[x, y]] += 1
       end
     end
   else
     a = (p2[0] - p1[0]) / (p2[1] - p1[1])
     b = -a * p1[0] + p1[1]
     (c[0].min..c[0].max).each do |x|
-      coords2.push("#{x},#{x * a + b}")
+      counts2[[x, x * a + b]] += 1
     end
   end
 end
-puts coords1.group_by { |i| i }.map { |k, v| v.count }.count { |v| v > 1 }
-puts coords2.group_by { |i| i }.map { |k, v| v.count }.count { |v| v > 1 }
+puts counts1.count { |c| c[1] > 1 }
+puts counts2.count { |c| c[1] > 1 }
